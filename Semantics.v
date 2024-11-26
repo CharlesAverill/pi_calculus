efcf14d671d391be98aa88bc385f9dd78a5a9c1c
Charles Averill <charlesaverill20@gmail.com>
2024-11-26 16:27:30 -0600

    Explain milner example

--- Semantics.v
+++ Semantics.v
@@ -201,6 +201,22 @@ Lemma Congr_Par_rotate : forall P Q R,
     <{P|Q|R}> =c= <{Q|R|P}>.
 Proof. eauto with picalc. Qed.
 
+(* Shows that the semantics are non-deterministic
+
+    The state
+        x<y>,# | x(u),u<v>,# | x<y>,#
+    can step into two states:
+        (1) # | y<v>,# | x<y>,#
+        (2) x<y>,# | z<v>,# | #
+    
+    In (1), the leftmost thread sends the message "y" on the channel "x" to the 
+    center thread. The center thread receives, binds its input to "u", and then 
+    waits to send on channel "u". After receiving the message from the leftmost
+    thread, the sent message "y" is substituted for all occurrences of "u".
+
+    In (2), the same thing happens, but it is instead the rightmost thread that
+    sends to the center thread.
+*)
 Example milner_2_2 :
     (<{x<y>,# | x(u),u<v>,# | x<y>,#}> ~>* <{# | y<v>,# | x<y>,#}>) /\
     (<{x<y>,# | x(u),u<v>,# | x<y>,#}> ~>* <{x<y>,# | z<v>,# | #}>).
